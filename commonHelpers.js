e1a36782553c3048e1033612beb6284f52670b95
wamseky <140257778+wamseky@users.noreply.github.com>
2024-02-17 14:32:36 +0000

    Deploying to gh-pages from @ wamseky/goit-js-hw-10@fa38ac4fd0ee5d8ef45634b1da821b8a9896d5c3 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/styles-348fb92f.js";import{f as r,i as a}from"./assets/vendor-77e16229.js";new r("#datetime-picker");new a("#datetime-picker");function t(o){const n=Math.floor(o/864e5),e=Math.floor(o%864e5/36e5),c=Math.floor(o%864e5%36e5/6e4),s=Math.floor(o%864e5%36e5%6e4/1e3);return{days:n,hours:e,minutes:c,seconds:s}}console.log(t(2e3));console.log(t(14e4));console.log(t(2414e4));
+//# sourceMappingURL=commonHelpers.js.map
