777a9f94bad43b426df9416bc77db1168a8177bc
Diana Kharchenko <vitadiana2@gmail.com>
2024-02-19 15:30:51 +0200

    timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,38 +1,112 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-const timer = new flatpickr('#datetime-picker')
-const message = new iziToast('#datetime-picker')
-const options = {
+import 'izitoast/dist/css/iziToast.min.css';
+
+
+const stopTimer = (selectedDate, intervalID) => {
+  if (selectedDate < Date.now() + 400) {
+    clearInterval(intervalID);
+    intervalID = null;
+  }
+}
+
+const convertMs = (ms) => {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return initTimer(days, hours, minutes, seconds);
+}
+
+const initTimer = (days, hours, minutes, seconds) => {
+  const daysTarget = document.querySelector('[data-days]');
+  const hoursTarget = document.querySelector('[data-hours]');
+  const minutesTarget = document.querySelector('[data-minutes]');
+  const secondsTarget = document.querySelector('[data-seconds]');
+
+  daysTarget.textContent = days.toString().padStart(2, '0');
+  hoursTarget.textContent = hours.toString().padStart(2, '0');
+  minutesTarget.textContent = minutes.toString().padStart(2, '0');
+  secondsTarget.textContent = seconds.toString().padStart(2, '0');
+}
+
+const errorMessage = () => {
+  iziToast.error({
+    class: 'popup-message',
+    theme: 'dark',
+    backgroundColor: '#ef4040',
+    messageColor: '#fff',
+    position: 'topRight',
+    pauseOnHover: true,
+    timeout: 3000,
+    message: 'Please choose a date in the future'
+  });
+}
+
+const mobileInputAttrs = (state) => {
+  const input = document.querySelector('.flatpickr-input.flatpickr-mobile');
+
+  if (!input) {
+    return;
+  }
+
+  state ? input.setAttribute('disabled', '') : input.removeAttribute('disabled');
+}
+
+const timerHandler = (selectedDate, input, trigger) => {
+  trigger.removeAttribute('disabled');
+  input.removeAttribute('disabled');
+  mobileInputAttrs(false);
+  let intervalID;
+
+  trigger.addEventListener('click', () => {
+    trigger.setAttribute('disabled', '');
+    input.setAttribute('disabled', '');
+    mobileInputAttrs(true);
+
+    intervalID = setInterval(() => {
+      convertMs(selectedDate - Date.now());
+      stopTimer(selectedDate, intervalID);
+    }, 200)
+  });
+}
+
+const initInputTimer = () => {
+  const input = document.querySelector('#datetime-picker');
+  const trigger = document.querySelector('[data-start]');
+  let userSelectedDate;
+
+  if (!input || !trigger) {
+    return;
+  }
+
+  trigger.setAttribute('disabled', '');
+
+  const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
+    dateFormat: 'Y-m-d h:m',
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
+      userSelectedDate = selectedDates[0];
+      if (userSelectedDate <= Date.now() || isNaN(userSelectedDate)) {
+        trigger.setAttribute('disabled', '');
+        errorMessage();
+      } else {
+        timerHandler(userSelectedDate, input, trigger);
+      }
+    }
   };
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-    
+
+  flatpickr(input, options);
+}
+
+initInputTimer();
